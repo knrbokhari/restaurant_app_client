d1b073aa995ba6bb76885b1b0a0205b8df544fcf
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2023-02-27 18:55:34 +0600

    addProduct page update

--- src/pages/dashboard/product/AddProduct.js
+++ src/pages/dashboard/product/AddProduct.js
@@ -1,110 +1,179 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { TextField, Select, Box, Grid, Stack, Button, Autocomplete, Chip } from '@mui/material';
+import {
+    TextField,
+    Paper,
+    Select,
+    Box,
+    Grid,
+    Stack,
+    Button,
+    Autocomplete,
+    Chip,
+    InputLabel,
+    OutlinedInput,
+    FormHelperText,
+    Typography
+} from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const AddProduct = () => {
-    const options = [{ value: 'Normal' }, { value: 'Medium' }, { value: 'Large' }];
-
     return (
         <>
             <Box>
-                <Formik
-                    initialValues={{
-                        name: '',
-                        price: 0,
-                        description: '',
-                        time: 0,
-                        discount: 0,
-                        size: []
-                        // image: []
-                    }}
-                    validationSchema={Yup.object().shape({
-                        // name: Yup.string().required('Name is required'),
-                        // price: Yup.number().required('Price is required'),
-                        // description: Yup.string().required('Description is required'),
-                        // time: Yup.number().required('Time is required'),
-                        // discount: Yup.number().required('Discount is required'),
-                        size: Yup.object().shape({
-                            value: Yup.string().required('Required')
-                        })
-                        // image: Yup.array().of(Yup.string()).required('Image is required')
-                    })}
-                    onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
-                        try {
-                            setStatus({ success: false });
-                            setSubmitting(false);
-                            console.log(values);
-                        } catch (err) {
-                            setStatus({ success: false });
-                            setErrors({ submit: err.message });
-                            setSubmitting(false);
-                        }
-                    }}
-                >
-                    {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
-                        <form noValidate onSubmit={handleSubmit}>
-                            <Grid container spacing={3}>
-                                <Grid item xs={12} md={6}>
-                                    <Field component={TextField} name="name" type="text" label="Name" margin="normal" fullWidth />
-                                    <ErrorMessage name="name" component="div" />
+                <Typography>Add Product</Typography>
+                <Paper variant="outlined" sx={{ padding: '30px' }}>
+                    <Formik
+                        initialValues={{
+                            name: '',
+                            price: 0,
+                            description: '',
+                            time: 0,
+                            discount: 0,
+                            image: []
+                        }}
+                        validationSchema={Yup.object().shape({
+                            name: Yup.string().required('Name is required'),
+                            price: Yup.number().required('Price is required'),
+                            description: Yup.string().required('Description is required'),
+                            time: Yup.number().required('Time is required'),
+                            discount: Yup.number().required('Discount is required'),
+                            image: Yup.array().of(Yup.string()).required('Image is required')
+                        })}
+                        onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+                            try {
+                                setStatus({ success: false });
+                                setSubmitting(false);
+                                console.log(values);
+                            } catch (err) {
+                                setStatus({ success: false });
+                                setErrors({ submit: err.message });
+                                setSubmitting(false);
+                            }
+                        }}
+                    >
+                        {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+                            <form noValidate onSubmit={handleSubmit}>
+                                <Grid container spacing={3}>
+                                    <Grid item xs={12} md={6}>
+                                        <Stack spacing={1}>
+                                            <InputLabel htmlFor="email-login">Product Name:</InputLabel>
+                                            <OutlinedInput
+                                                id="email-login"
+                                                type="email"
+                                                value={values.name}
+                                                name="name"
+                                                onBlur={handleBlur}
+                                                onChange={handleChange}
+                                                placeholder="Enter name address"
+                                                fullWidth
+                                                error={Boolean(touched.name && errors.name)}
+                                            />
+                                            {touched.name && errors.name && (
+                                                <FormHelperText error id="standard-weight-helper-text-name-login">
+                                                    {errors.name}
+                                                </FormHelperText>
+                                            )}
+                                        </Stack>
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <Stack spacing={1}>
+                                            <InputLabel htmlFor="email-login">Price:</InputLabel>
+                                            <OutlinedInput
+                                                id="email-login"
+                                                type="text"
+                                                value={values.price}
+                                                name="price"
+                                                onBlur={handleBlur}
+                                                onChange={handleChange}
+                                                placeholder="Enter Price"
+                                                fullWidth
+                                                error={Boolean(touched.price && errors.price)}
+                                            />
+                                            {touched.price && errors.price && (
+                                                <FormHelperText error id="standard-weight-helper-text-name-login">
+                                                    {errors.price}
+                                                </FormHelperText>
+                                            )}
+                                        </Stack>
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <Stack spacing={1}>
+                                            <InputLabel htmlFor="email-login">Time (min):</InputLabel>
+                                            <OutlinedInput
+                                                id="email-login"
+                                                type="number"
+                                                value={values.time}
+                                                name="time"
+                                                onBlur={handleBlur}
+                                                onChange={handleChange}
+                                                placeholder="Enter Time"
+                                                fullWidth
+                                                error={Boolean(touched.time && errors.time)}
+                                            />
+                                            {touched.time && errors.time && (
+                                                <FormHelperText error id="standard-weight-helper-text-name-login">
+                                                    {errors.time}
+                                                </FormHelperText>
+                                            )}
+                                        </Stack>
+                                    </Grid>
+                                    <Grid item xs={12} md={6}>
+                                        <Stack spacing={1}>
+                                            <InputLabel htmlFor="email-login">Discount:</InputLabel>
+                                            <OutlinedInput
+                                                id="email-login"
+                                                type="number"
+                                                value={values.discount}
+                                                name="discount"
+                                                onBlur={handleBlur}
+                                                onChange={handleChange}
+                                                placeholder="Enter Discount"
+                                                fullWidth
+                                                error={Boolean(touched.discount && errors.discount)}
+                                            />
+                                            {touched.discount && errors.discount && (
+                                                <FormHelperText error id="standard-weight-helper-text-name-login">
+                                                    {errors.discount}
+                                                </FormHelperText>
+                                            )}
+                                        </Stack>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Stack spacing={1}>
+                                            <InputLabel htmlFor="email-login">Description:</InputLabel>
+                                            <OutlinedInput
+                                                id="email-login"
+                                                type="text"
+                                                multiline
+                                                rows={5}
+                                                value={values.description}
+                                                name="description"
+                                                onBlur={handleBlur}
+                                                onChange={handleChange}
+                                                placeholder="Enter Description"
+                                                fullWidth
+                                                error={Boolean(touched.description && errors.description)}
+                                            />
+                                            {touched.description && errors.description && (
+                                                <FormHelperText error id="standard-weight-helper-text-name-login">
+                                                    {errors.description}
+                                                </FormHelperText>
+                                            )}
+                                        </Stack>
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="primary">
+                                            Create
+                                        </Button>
+                                    </Grid>
                                 </Grid>
-                                <Grid item xs={12} md={6}>
-                                    <Field component={TextField} name="price" type="number" label="Price" margin="normal" fullWidth />
-                                    <ErrorMessage name="price" component="div" />
-                                </Grid>
-                                <Grid item xs={12} md={6}>
-                                    <Field component={TextField} name="time" type="number" label="Time" margin="normal" fullWidth />
-                                    <ErrorMessage name="time" component="div" />
-                                </Grid>
-                                <Grid item xs={12} md={6}>
-                                    <Field component={TextField} name="discount" type="number" label="Discount" margin="normal" fullWidth />
-                                    <ErrorMessage name="discount" component="div" />
-                                </Grid>
-                                {/* <Grid item xs={12}></Grid> */}
-                                <Grid item xs={12}>
-                                    <TextField
-                                        select
-                                        label="Select Options"
-                                        name="selectedOptions"
-                                        multiple
-                                        value={values.size}
-                                        onChange={(e) =>
-                                            Array.from(e.target.size, (item) => ({
-                                                value: item.value
-                                            }))
-                                        }
-                                        helperText={touched.selectedOptions ? errors.selectedOptions : ''}
-                                        error={Boolean(touched.selectedOptions && errors.selectedOptions)}
-                                        fullWidth
-                                    >
-                                        {options.map((option) => (
-                                            <option key={option.value} value={option.value}>
-                                                {option.value}
-                                            </option>
-                                        ))}
-                                    </TextField>
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Field
-                                        component={TextField}
-                                        name="description"
-                                        type="text"
-                                        label="Description"
-                                        margin="normal"
-                                        fullWidth
-                                    />
-                                    <ErrorMessage name="description" component="div" />
-                                </Grid>
-                                <Grid item xs={12}>
-                                    <Button disableElevation fullWidth size="large" type="submit" variant="contained" color="primary">
-                                        Create
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </form>
-                    )}
-                </Formik>
+                            </form>
+                        )}
+                    </Formik>
+                </Paper>
             </Box>
         </>
     );
